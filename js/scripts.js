e8500410cdf9b3a144aac1802a308f5806efe87f
mowenpark <michaelowenpark@gmail.com>
2016-07-15 16:25:53 -0700

    fix js easing

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 (function($) {
 
     $('.page-scroll').bind('click', function(event) {
-        var target = $( $(this).attr('href') );
-        $('html, body').stop().animate({
-            scrollTop: (target.offset().top - 60),
-            duration: 2000,
-            easing: 'easeInOutQuad'});
-        event.preventDefault();
+
+          var $ele = $(this);
+          // debugger;
+          $('html, body').stop().animate({
+              scrollTop: ($($ele.attr('href')).offset().top - 60)
+          }, 1000, 'swing');
+          event.preventDefault();
+
     });
 
 })(jQuery);
